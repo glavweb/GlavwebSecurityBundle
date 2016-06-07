f862d99e0ab5cba88748e0a5a10277a835b06953
nilov <nilov@glavweb.ru>
2016-06-07 11:56:53 +0600

    Add SuperAdminVoter.

---
+++ Security/Authorization/Voter/SuperAdminVoter.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the Glavweb SecurityBundle package.
+ *
+ * (c) GLAVWEB <info@glavweb.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glavweb\SecurityBundle\Security\Authorization\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class SuperAdminVoter
+ *
+ * @author Andrey Nilov <nilov@glavweb.ru>
+ * @package Glavweb\SecurityBundle
+ */
+class SuperAdminVoter implements VoterInterface
+{
+    /**
+     * Name of super admin role
+     */
+    const SUPER_ADMIN_ROLE = 'ROLE_SUPER_ADMIN';
+
+    /**
+     * Checks if the voter supports the given attribute.
+     *
+     * @param string $attribute An attribute
+     *
+     * @return bool true if this Voter supports the attribute, false otherwise
+     */
+    public function supportsAttribute($attribute)
+    {
+        return true;
+    }
+
+    /**
+     * Checks if the voter supports the given class.
+     *
+     * @param string $class A class name
+     *
+     * @return bool true if this Voter can process the class
+     */
+    public function supportsClass($class)
+    {
+        return true;
+    }
+
+    /**
+     * Returns the vote for the given parameters.
+     *
+     * This method must return one of the following constants:
+     * ACCESS_GRANTED, ACCESS_DENIED, or ACCESS_ABSTAIN.
+     *
+     * @param TokenInterface $token      A TokenInterface instance
+     * @param object|null $object     The object to secure
+     * @param array $attributes An array of attributes associated with the method being invoked
+     *
+     * @return int either ACCESS_GRANTED, ACCESS_ABSTAIN, or ACCESS_DENIED
+     */
+    public function vote(TokenInterface $token, $object, array $attributes)
+    {
+        /** @var UserInterface $user */
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return VoterInterface::ACCESS_ABSTAIN;
+        }
+
+        $roles = $user->getRoles();
+        if (in_array(self::SUPER_ADMIN_ROLE, $roles)) {
+            return VoterInterface::ACCESS_GRANTED;
+        }
+
+        return VoterInterface::ACCESS_ABSTAIN;
+    }
+}
